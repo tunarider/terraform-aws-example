29c78b4ab10da69ddf0f6873a638fec14766f6ca
tunarider <tunarider@tunaground.net>
2021-02-08 19:23:03 +0900

    key pair 이름 정리

--- key_pairs.tf
+++ key_pairs.tf
@@ -1,20 +1,20 @@
 resource "aws_key_pair" "default" {
-  key_name   = "default"
+  key_name   = "${var.project_name}-default"
   public_key = var.public_key_default
 
   tags = {
     Provisioner = "terraform"
     Project     = var.project_name
-    Name        = "default"
+    Name        = "${var.project_name}-default"
   }
 }
 
 resource "aws_key_pair" "bastion" {
-  key_name   = "bastion"
+  key_name   = "${var.project_name}-bastion"
   public_key = var.public_key_bastion
   tags = {
     Provisioner = "terraform"
     Project     = var.project_name
-    Name        = "bastion"
+    Name        = "${var.project_name}-bastion"
   }
 }
